6c198f2af6d75d0610d3577ef28b9dc0d7da3917
Meni Samet <meni432@gmail.com>
2016-05-02 18:16:40 +0300

    update files initials

--- YanivServerTCP/src/TCPServer.java
+++ YanivServerTCP/src/TCPServer.java
@@ -1,90 +1,104 @@
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * The class extends the Thread class so we can receive and send messages at the same time
+ * The class extends the Thread class so we can receive and send messages at the
+ * same time
  */
 public class TCPServer extends Thread {
- 
+
     public static final int SERVERPORT = 4444;
     private boolean running = false;
     private PrintWriter mOut;
     private OnMessageReceived messageListener;
- 
-    public static void main(String[] args) {
- 
-        //opens the window where the messages will be received and sent
 
-        TCPServer tcpServer;
-        tcpServer = new TCPServer(new OnMessageReceived() {
-            @Override
-            public void messageReceived(String message) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        });
-        tcpServer.start();
-    }
- 
+    static int id = 1;
+
+    private ArrayList<Player> players = new ArrayList<>();
+
     /**
      * Constructor of the class
+     *
      * @param messageListener listens for the messages
      */
     public TCPServer(OnMessageReceived messageListener) {
         this.messageListener = messageListener;
     }
- 
+
     /**
      * Method to send the messages from server to client
+     *
      * @param message the message sent by the server
      */
-    public void sendMessage(String message){
+    public void sendMessage(String message) {
         if (mOut != null && !mOut.checkError()) {
             mOut.println(message);
             mOut.flush();
         }
     }
- 
+
     @Override
     public void run() {
         super.run();
- 
+
         running = true;
- 
+
         try {
             System.out.println("S: Connecting...");
- 
+
             //create a server socket. A server socket waits for requests to come in over the network.
-            ServerSocket serverSocket = new ServerSocket(SERVERPORT);
- 
-            //create client socket... the method accept() listens for a connection to be made to this socket and accepts it.
+            final ServerSocket serverSocket = new ServerSocket(SERVERPORT);
+
             Socket client = serverSocket.accept();
-            System.out.println("S: Receiving...");
- 
+
+            new Thread("Listener Thread") {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            Socket client = serverSocket.accept();
+//                            players.add(new Player(client, id++));
+                        } catch (IOException ex) {
+                            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+
+                }
+            }.start();
+
             try {
- 
-                //sends the message to the client
-                mOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
- 
-                //read the message received from client
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
- 
-                //in this while we wait to receive messages from client (it's an infinite loop)
-                //this while it's like a listener for messages
-                while (running) {
-                    String message = in.readLine();
- 
-                    if (message != null && messageListener != null) {
-                        //call the method messageReceived from ServerBoard class
-                        messageListener.messageReceived(message);
+
+                for (Player player : players) {
+                    //sends the message to the client
+                    mOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
+
+                    //read the message received from client
+                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                    //in this while we wait to receive messages from client (it's an infinite loop)
+                    //this while it's like a listener for messages
+                    while (running) {
+                        String message = in.readLine();
+
+                        if (message != null && messageListener != null) {
+                            //call the method messageReceived from ServerBoard class
+                            messageListener.messageReceived(message);
+                        }
                     }
                 }
- 
+
             } catch (Exception e) {
                 System.out.println("S: Error");
                 e.printStackTrace();
@@ -92,18 +106,32 @@ public class TCPServer extends Thread {
                 client.close();
                 System.out.println("S: Done.");
             }
- 
+
         } catch (Exception e) {
             System.out.println("S: Error");
             e.printStackTrace();
         }
- 
+
     }
- 
+
     //Declare the interface. The method messageReceived(String message) will must be implemented in the ServerBoard
     //class at on startServer button click
     public interface OnMessageReceived {
+
         public void messageReceived(String message);
     }
- 
+
+    public static void main(String[] args) {
+
+        //opens the window where the messages will be received and sent
+        TCPServer tcpServer;
+        tcpServer = new TCPServer(new OnMessageReceived() {
+            @Override
+            public void messageReceived(String message) {
+                System.out.println("message recived :" + message);
+            }
+        });
+        tcpServer.start();
+    }
+
 }
